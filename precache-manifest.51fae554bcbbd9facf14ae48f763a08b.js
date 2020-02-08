ddd37954ac656a392b5c1aea07d3403dda0cf0d2
MoeBoukhriss <mohammed.boukhriss@outlook.com>
2020-02-08 18:52:27 -0500

    Updates

---
+++ precache-manifest.51fae554bcbbd9facf14ae48f763a08b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55af440a0a6dcb624c1e99eaf86b3b6a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "556583c3c97ebe6e6828",
+    "url": "/monsters-rolodex/static/css/main.fd535608.chunk.css"
+  },
+  {
+    "revision": "01fa59518f48c0c9b0f6",
+    "url": "/monsters-rolodex/static/js/2.650d6c94.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.650d6c94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "556583c3c97ebe6e6828",
+    "url": "/monsters-rolodex/static/js/main.948878c6.chunk.js"
+  },
+  {
+    "revision": "420e781ce2f2f4d61faa",
+    "url": "/monsters-rolodex/static/js/runtime-main.9cf5dfda.js"
+  }
+]);
